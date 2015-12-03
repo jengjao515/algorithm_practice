29153c387babeea9bfa4451719fde7d1bf2c47d1
Jennifer Jaochico <jenjao515@gmail.com>
2015-12-03 01:41:29 -0800

    added tapeequilibrium, still needs edge case fixing

--- javascript/tapeequilibrium.js
+++ javascript/tapeequilibrium.js
@@ -2,27 +2,29 @@
 // 1. Save values to a separate array, save a place to find the difference
 // 2. if one difference is smaller than the other, return 
 
-function solution(numberArray) { 
-	var numbers = numberArray;
-	var total = numbers.reduce(function(a, b) {return a + b;});
+var solution = function(nums) { 
+	var numbers = nums;
+	var firstHalf = numbers[0];
+	var total = (numbers.reduce(function(a, b) {return a + b;})) - firstHalf;
 	var result = new Array();
 	
 	for (var i = 0; i < numbers.length; i++) {
-		var subtractMe = numbers[i];
-		var difference = total - subtractMe;
-		if (difference == result[0] || difference)
-		result.push(difference);
+		if (i === 0) { 
+			result[0] = Math.abs(firstHalf - total);
+		}
+		else { 
+			firstHalf = firstHalf + numbers[i];
+			total = total - firstHalf;
+			var difference = Math.abs(firstHalf - total);
+			if (difference < result[0]) {
+				result[0] = difference;
+				result[1] = i;
+			};
+		};
 		i + 1;
 	};
-
-	console.log(result);
-	for (var i=0; i < results.length; i++) { 
-		var num = results[i]
-		if num 
-
-	};
-
+	return numbers[result[1]];
 };
 
-var test_array = [1, 2, 3];
+var test_array = [3, 1, 2, 4, 3];
 solution(test_array);
